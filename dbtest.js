549c5b46c33e88316449a540210b7892d58c1edf
Marc Wood <marcdw87@gmail.com>
2019-01-18 23:07:57 -0600

    Initial commit.

---
+++ dbtest.js
@@ -0,0 +1,42 @@
+/* eslint-env nashorn */
+/* global importClass Files Paths Charset DriverManager */
+
+importClass(java.nio.file.Files);
+importClass(java.nio.file.Paths);
+importClass(java.nio.charset.Charset);
+importClass(java.sql.DriverManager);
+
+const jdbcUrl = 'jdbc:mysql://db4free.net/rhinotest?user=cypher&password=dbpassword';
+const conn = DriverManager.getConnection(jdbcUrl);
+const stat = conn.createStatement();
+
+const outputLines = [];
+let resultSet;
+try {
+  resultSet = stat.executeQuery('SELECT * FROM People;');
+
+  while (resultSet.next()) {
+    let name = resultSet.getString('name');
+    let occupation = resultSet.getString('occupation');
+    outputLines.push(name + ',' + occupation);
+  }
+} catch (e) {
+  e.javaException instanceof java.sql.SQLException &&
+    print('A database access error occurred.');
+  e.javaException instanceof java.sql.SQLTimeoutException &&
+    print('The driver has determined that the timeout value has been exceeded.');
+} finally {
+  resultSet.close();
+  stat.close();
+  conn.close();
+}
+
+try {
+  const outputFile = Paths.get('outfile.csv');
+  Files.write(outputFile, outputLines, Charset.forName('UTF-8'));
+} catch (e) {
+  e.javaException instanceof java.nio.file.InvalidPathException &&
+    print('The path string cannot be converted to a Path.');
+  e.javaException instanceof java.io.IOException &&
+    print('An I/O error occurred writing to or creating the file.');
+}
